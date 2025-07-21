53243e4f3d4a6623f73905c4f27b979efa3b3b98
Ali Barznji <53957149+Ali559@users.noreply.github.com>
2025-07-22 00:47:04 +0300

    initial release

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  // Your custom configs here
+  {
+    rules: {
+      // Your custom rules here
+      "no-console": "error",
+      "no-debugger": "error",
+      "no-unused-vars": "error",
+      "vue/no-unused-properties": "error",
+      "vue/no-unused-components": "error",
+      "vue/no-unused-vars": "error",
+    },
+  },
+);
