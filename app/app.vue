30ecc6a79c042f1d1465dbbb6b1136cadb6bae9c
Ali Barznji <53957149+Ali559@users.noreply.github.com>
2025-07-26 01:14:38 +0300

    added speed insights

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import LinksString from "./components/LinksString.vue";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 </script>
 
 <template>
   <UApp>
     <LinksString />
     <NuxtLayout>
+      <SpeedInsights />
       <NuxtPage />
     </NuxtLayout>
   </UApp>
