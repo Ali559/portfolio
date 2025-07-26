e47cf6f8fc41f1148c47d5862d23239281ee1d3f
Ali Barznji <53957149+Ali559@users.noreply.github.com>
2025-07-27 00:46:59 +0300

    bug fix

--- app/components/MainButton.vue
+++ app/components/MainButton.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink
-    :to="to.startsWith('https') ? to : ``"
+    :to="to.startsWith('/') ? to : ``"
     class="border-[#C778DD] border-1 py-2 px-4 text-white font-medium hover:bg-[#C778DD] transition-colors duration-500 ease-in-out rounded-xs cursor-pointer"
   >
     {{ text }}
---
+++ app/pages/About.vue
@@ -0,0 +1,8 @@
+
+<template>
+    <div class="@container">
+        <h1 class="text-3xl font-semibold text-white">
+            <span class="text-primary">/</span>about
+        </h1>
+    </div>
+</template>
